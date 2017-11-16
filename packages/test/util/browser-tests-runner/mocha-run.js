67dfc0c908b40cdec496b1c356afd81103d13c26
Austin Kelleher <austin.kell47@gmail.com>
2017-11-16 11:56:05 -0500

    Initial commit

---
+++ packages/test/util/browser-tests-runner/mocha-run.js
@@ -0,0 +1,14 @@
+const runner = window.mocha.run();
+
+let success = true;
+
+runner.once("fail", () => {
+  success = false;
+});
+
+runner.on("end", event => {
+  console.log("result:", {
+    success,
+    coverage: window.__coverage__
+  });
+});
